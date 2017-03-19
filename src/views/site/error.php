a1a052660b2aa50802d64cbf4c8315f7a7b72a8e
Dmytro Karpovych <ZAYEC77@gmail.com>
2017-03-19 23:21:02 +0200

    fix main menu toggling

--- src/views/site/error.php
+++ src/views/site/error.php
@@ -11,7 +11,13 @@ $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                <?= Html::encode($this->title) ?>
+            </h1>
+        </div>
+    </div>
 
     <div class="alert alert-danger">
         <?= nl2br(Html::encode($message)) ?>
