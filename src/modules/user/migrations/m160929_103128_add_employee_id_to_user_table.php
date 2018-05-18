5ca0acc09fed631f229a3b2d2b098f74e757e5b6
Vyacheslav Karpovych <vyacheslav.karpovych@gmail.com>
2018-05-18 23:54:02 +0300

    add employee_id to user table

---
+++ src/modules/user/migrations/m160929_103128_add_employee_id_to_user_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Migration;
+
+class m160929_103128_add_employee_id_to_user_table extends Migration
+{
+  public function up()
+  {
+    $this->addColumn('{{%user}}', 'employee_id', $this->integer());
+
+  }
+
+  public function down()
+  {
+    $this->dropColumn('{{%user}}', 'employee_id');
+  }
+}
