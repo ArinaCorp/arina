d3ee95ca8d9a7411c2bb40837d9816d375f5ae5b
Dmytro Karpovych <ZAYEC77@gmail.com>
2018-05-19 00:13:50 +0300

    add employee_id to user model

--- src/modules/user/models/User.php
+++ src/modules/user/models/User.php
@@ -15,11 +15,36 @@ use Yii;
 /**
  * Class User
  *
+ * @property integer $employee_id
  *
  * @package app\modules\user\models
  */
 class User extends BaseUser
 {
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return array_merge(parent::rules(), [
+            'employee_id' => ['employee_id', 'integer'],
+        ]);
+    }
+
+    /**
+     * @return array
+     */
+    public function attributeLabels()
+    {
+        return array_merge(parent::attributeLabels(), [
+            'employee_id' => \Yii::t('user', 'Employee'),
+        ]);
+    }
+
+
+    /**
+     * @return \yii\rbac\Role[]
+     */
     public function getRoles()
     {
         return Yii::$app->authManager->getRolesByUser($this->id);
