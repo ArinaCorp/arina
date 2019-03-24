18199dc207b2e5245ca5fd6f983e14d0bbc9a445
wenceslaus <esteticwence@gmail.com>
2019-03-24 23:45:46 +0200

    Add migrations

---
+++ src/modules/directories/migrations/m190324_213550_create_subject_block_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `subject_block`.
+ */
+class m190324_213550_create_subject_block_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('subject_block', [
+            'id' => $this->primaryKey(11),
+            'speciality_id' => $this->integer(11),
+            'course' => $this->integer(11),
+            'created' => $this->timestamp()->defaultValue('CURRENT_TIMESTAMP'),
+        ]);
+
+        $this->createIndex(
+            'idx-subject_block-speciality_id',
+            'subject_block',
+            'speciality_id'
+        );
+
+        $this->addForeignKey(
+            'fk-subject_block-speciality_id',
+            'subject_block',
+            'speciality_id',
+            'speciality',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('subject_block');
+    }
+}
